0fee4f4998842825a0ed38999f38e86a6ba491b6
bajarappa-business <bajarappa.business@gmail.com>
2024-09-02 16:51:29 +0530

    generating dynamic metadata

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,6 +1,11 @@
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { getCabin } from "@/app/_lib/data-service";
 
+export async function generateMetadata({ params }) {
+  const { name } = await getCabin(params.cabinId);
+  return { title: `cabin ${name}` };
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
